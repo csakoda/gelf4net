95d97bf2a1c53e072d8789e73805c199eb4467b3
Chuck Sakoda <chuck@appature.com>
2013-04-30 14:50:18 -0700

    Add simple RabbitMQ appender
    Add RabbitMQ appender (with no real features) that just writes to an
    exchange instead of to a specific queue.
    
    Future would would merge this into the GelfAmqpAppender, providing more
    specific semantics for choosing whether you want to write to an exchange
    or queue, what type the exchange might be, whether you want to create
    the exchange or the queue.

---
+++ src/Gelf4net/Appender/GelfRabbitMQAppender.cs
@@ -0,0 +1,31 @@
+﻿using log4net.Appender;
+using log4net.Util;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace gelf4net.Appender
+{
+    public class GelfRabbitMQAppender : GelfAmqpAppender
+    {
+        public GelfRabbitMQAppender(): base() { }
+
+        public string RemoteExchange { get; set; }
+        
+        protected override void Append(log4net.Core.LoggingEvent loggingEvent)
+        {
+            var message = RenderLoggingEvent(loggingEvent).GzipMessage(Encoding);
+
+            using (IConnection conn = ConnectionFactory.CreateConnection())
+            {
+                var model = conn.CreateModel();
+                model.ExchangeDeclare(this.RemoteExchange, ExchangeType.Topic);
+                byte[] messageBodyBytes = message;
+                model.BasicPublish(RemoteExchange, "key", null, messageBodyBytes);
+            }
+        }
+    }
+}
